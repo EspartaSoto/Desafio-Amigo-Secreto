0e6549a8d5d0670a11a973acf42e5f5047f7b2fe
Daniel <daniel.sotoliva@gmail.com>
2025-03-18 15:36:45 -0300

    actualizacion de archivos

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[]
+
+function agregarAmigo() {
+    const inputAmigo = document.getElementById("amigo");
+    const nombreAmigo = inputAmigo.value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    inputAmigo.value = "";
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        const amigo = amigos[i];
+        const nuevoItemLista = document.createElement("li");
+        nuevoItemLista.textContent = amigo;
+        listaAmigos.appendChild(nuevoItemLista);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear. Agrega amigos primero.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultadoElement = document.getElementById("resultado");
+    resultadoElement.innerHTML = `El amigo secreto es: <span>${amigoSorteado}</span>`;
+}
